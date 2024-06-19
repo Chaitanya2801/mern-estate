de99dc25cc5146bb093985453e1ab954a9010d21
Chaitanya2801 <chetan777agarwal@gmail.com>
2024-06-19 16:02:07 -0700

    sign-in API created and tested

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -14,4 +15,24 @@ export const signup = async (req, res, next) => {
     }
 };
 
-export default signup;
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) {
+            return next(errorHandler(404, "User not found"));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) {
+            return next(errorHandler(401, "Invalid Credentials"));
+        }
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET); // creating token using unique id and constant value
+        const {password: pass, ...rest} = validUser._doc; //taking out password
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+};
