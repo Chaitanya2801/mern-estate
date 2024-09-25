9953aa50b45dc78d6f59781e4fab4930c33482a3
Chaitanya2801 <chetan777agarwal@gmail.com>
2024-09-26 03:42:15 +0530

    OAuth file creatd for google sign In handling

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useNavigate, Link } from 'react-router-dom';
 import React, { useState } from 'react';
+import { OAuth } from '../components/OAuth';
 
 const SignUp = () => {
 
@@ -52,6 +53,7 @@ const SignUp = () => {
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? "Loading..." : "Sign Up"}
         </button>
+        <OAuth />
       </form>
 
       <div className='flex gap-2 mt-5'>
